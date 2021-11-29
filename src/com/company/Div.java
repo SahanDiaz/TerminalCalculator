5c8d1cfb636af9d1906fa293be866e30a9b06191
SahanDiaz <sahandiaz1@gmail.com>
2021-11-29 16:34:45 +0530

    division method completed

---
+++ src/com/company/Div.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Div {
+    public double division(double num1, double num2){
+        return num1/num2;
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,26 +13,38 @@ public class Main {
         while (x){
             System.out.print("**********  \nCalculator \n********** \n please enter : \n1 - Addition\n2 - Substraction\n3 - Multiply\n4 - Division\n_________\nEnter Choice : ");
             int input = sc.nextInt();
-            System.out.print("Enter first number : ");
-            num1 = sc.nextDouble();
-            System.out.print("Enter second number : ");
-            num2 = sc.nextDouble();
+            if(input>0 && input<5){
+                System.out.print("Enter first number : ");
+                num1 = sc.nextDouble();
+                System.out.print("Enter second number : ");
+                num2 = sc.nextDouble();
 
-            switch (input){
-                case 1:
-                    Add a1 = new Add();
-                    System.out.println("Answer : "+ a1.add(num1,num2));
-                    break;
-                case 2:
-                    Sub s1 = new Sub();
-                    System.out.println("Answer : "+ s1.sub(num1,num2));
-                    break;
-                case 3:
-                    Mul m1 = new Mul();
-                    System.out.println("Answer : "+ m1.multiply(num1,num2));
-                default:
-                    System.out.println("please enter correct number !!!");
+                switch (input){
+                    case 1:
+                        Add a1 = new Add();
+                        System.out.println("\nAnswer : "+ a1.add(num1,num2)+ "\n");
+                        break;
+                    case 2:
+                        Sub s1 = new Sub();
+                        System.out.println("\nAnswer : "+ s1.sub(num1,num2)+ "\n");
+                        break;
+                    case 3:
+                        Mul m1 = new Mul();
+                        System.out.println("\nAnswer : "+ m1.multiply(num1,num2)+ "\n");
+                        break;
+                    case 4:
+                        Div d1 = new Div();
+                        System.out.println("\nAnswer : "+ d1.division(num1,num2)+ "\n" );
+                        break;
+                    default:
+                        System.out.println("\nplease enter correct number !!!\n");
+                        break;
+                }
             }
+            else {
+                System.out.println("\nplease enter correct number !!!\n");
+            }
+
         }
 
     }
